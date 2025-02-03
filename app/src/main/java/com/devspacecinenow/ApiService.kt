590ceb494adda2b0d3e89e6e8aa2b20bab4c8b89
marcosantos <marcodossantoscontato@gmail.com>
2025-02-03 17:01:34 -0300

    Added DTO, Service, Client

---
+++ app/src/main/java/com/devspacecinenow/ApiService.kt
@@ -0,0 +1,11 @@
+package com.devspacecinenow
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiService {
+
+    @GET("now_playing?language=en-US&page=1")
+    fun getNowPlayingMovies() : Call<MovieResponse>
+
+}
