789a88842341a9fc3c4f3372cd99b8868ebffe46
marcosantos <marcodossantoscontato@gmail.com>
2025-02-11 16:40:14 -0300

    created movie session, new requests

--- app/src/main/java/com/devspacecinenow/ApiService.kt
+++ app/src/main/java/com/devspacecinenow/ApiService.kt
@@ -8,4 +8,13 @@ interface ApiService {
     @GET("now_playing?language=en-US&page=1")
     fun getNowPlayingMovies() : Call<MovieResponse>
 
+    @GET("top_rated?language=en-US&page=1")
+    fun getTopRatedMovies() : Call<MovieResponse>
+
+    @GET("popular?language=en-US&page=1")
+    fun getPopularMovies() : Call<MovieResponse>
+
+    @GET("upcoming?language=en-US&page=1")
+    fun getUpcomingMovies() : Call<MovieResponse>
+
 }
