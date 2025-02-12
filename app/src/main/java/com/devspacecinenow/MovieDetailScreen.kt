bc9f0ed13b1206649ff9003a0e934c011eb5d35b
marcosantos <marcodossantoscontato@gmail.com>
2025-02-12 17:33:22 -0300

    Navigation between screens, passing data between screens

---
+++ app/src/main/java/com/devspacecinenow/MovieDetailScreen.kt
@@ -0,0 +1,29 @@
+package com.devspacecinenow
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MovieDetailScreen(movieId: String) {
+    Text(text = movieId)
+    MovieDetailContent()
+}
+
+@Composable
+private fun MovieDetailContent() {
+    Column(
+            modifier = Modifier.fillMaxSize()
+    ) {
+            Text(text = "Movie Detail Screen")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MovieDetailPreview() {
+    MovieDetailContent()
+}
